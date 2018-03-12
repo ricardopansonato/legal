8a838c427a940d05438801c877bba33144b90c66
Ricardo Leonardo Pansonato <ricardopansonato@Ricardos-MacBook-Pro.local>
2018-03-12 20:04:17 -0300

    .

--- src/main/java/br/com/fiap/api/android/trabalho/controller/SwitchController.java
+++ src/main/java/br/com/fiap/api/android/trabalho/controller/SwitchController.java
@@ -4,12 +4,14 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.fiap.api.android.trabalho.repository.SwitchRepository;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class SwitchController {
 	
